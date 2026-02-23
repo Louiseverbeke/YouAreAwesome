f62a028559f17a55236e47208578c666bf332856
Louise Verbeke <louiseverbeke@yahoo.be>
2026-02-23 16:08:30 -0500

    after lesson 1.16

--- YouAreAwesome/ContentView.swift
+++ YouAreAwesome/ContentView.swift
@@ -43,45 +43,24 @@ struct ContentView: View {
             
             Spacer()
             
-                Button("Show Message") {
-                    let messages = ["You Are Awesome!",
-                                    "When the Genius Bar Needs Help, They Call You!",
-                                    "You Are Great",
-                                    "You are fantastic",
-                                    "Fabulous, that's You!",
-                                    "You Make Me Smile!",]
-                    
-                    var messageNumber: Int
-                    repeat {
-                        messageNumber = Int.random(in: 0..<messages.count-1)
-                    } while messageNumber == lastMessageNumber
-                message = messages[messageNumber]
-                    lastMessageNumber = messageNumber
-                    
-                    var imageNumber: Int
-                    repeat {
-                        imageNumber = Int.random(in: 0...(numberOfImages-1))
-                    } while imageNumber == lastImageNumber
-                    imageName = "image\(imageNumber)"
-                    lastImageNumber = imageNumber
-                    
-                    var soundNumber: Int
-                    repeat {
-                        soundNumber = Int.random(in: 0...(numberOfSounds-1))
-                    } while soundNumber == lastSoundNumber
-                    lastSoundNumber = soundNumber
-                    let soundName = "sound\(soundNumber)"
-                    
-                    guard let soundFile = NSDataAsset(name: soundName) else {
-                        print("😡 Could not read file named \(soundName)")
-                        return
-                    }
-                    do {
-                        audioPlayer = try AVAudioPlayer(data: soundFile.data)
-                        audioPlayer.play()
-                    } catch {
-                        print("😡 ERROR: \(error.localizedDescription) creating audioPlayer")
-                    }
+            Button("Show Message") {
+                let messages = ["You Are Awesome!",
+                                "When the Genius Bar Needs Help, They Call You!",
+                                "You Are Great",
+                                "You are fantastic",
+                                "Fabulous, that's You!",
+                                "You Make Me Smile!",]
+                
+                lastMessageNumber = nonRepeatingRandom(lastNumber: lastMessageNumber, upperBound: messages.count-1)
+                message = messages[lastMessageNumber]
+                
+                
+                lastImageNumber = nonRepeatingRandom(lastNumber: lastImageNumber, upperBound: numberOfImages-1)
+                imageName = "image\(lastImageNumber)"
+                
+                
+                lastSoundNumber = nonRepeatingRandom(lastNumber: lastSoundNumber, upperBound: numberOfSounds-1)
+                playSound(soundName: "sound\(lastSoundNumber)")
             }
             .buttonStyle(.borderedProminent)
             .font(.title2)
@@ -89,6 +68,27 @@ struct ContentView: View {
         }
         .padding()
         
+    }
+    
+    func nonRepeatingRandom(lastNumber: Int, upperBound: Int) -> Int {
+        var newNumber: Int
+        repeat {
+            newNumber = Int.random(in: 0...upperBound)
+        } while newNumber == lastNumber
+        return newNumber
+    }
+    
+    func playSound(soundName: String) {
+        guard let soundFile = NSDataAsset(name: soundName) else {
+            print("😡 Could not read file named \(soundName)")
+            return
+        }
+        do {
+            audioPlayer = try AVAudioPlayer(data: soundFile.data)
+            audioPlayer.play()
+        } catch {
+            print("😡 ERROR: \(error.localizedDescription) creating audioPlayer")
+        }
     }
 }
 
